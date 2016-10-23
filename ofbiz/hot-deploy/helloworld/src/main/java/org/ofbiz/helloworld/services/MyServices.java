59ae2f6b0a41b2f9715e0dc1d5dff6eb902e5848
1010836 <1010836@isep.ipp.pt>
2016-10-23 07:36:35 +0100

    ODSOFT - Exercise 4 - ref #6 Addiction of the new service createRecipe to the HelloWordl componente

--- ofbiz/hot-deploy/helloworld/src/main/java/org/ofbiz/helloworld/services/MyServices.java
+++ ofbiz/hot-deploy/helloworld/src/main/java/org/ofbiz/helloworld/services/MyServices.java
@@ -15,14 +15,36 @@ public class MyServices
   	// LocalDispatcher dispatcher = dctx.getDispatcher () ;
   	// Get the msg input parameter
   	String msg = (String)context.get("msg");
+  	
   	String msgUpper=msg.toUpperCase();
   	
   	Map<String, Object> result = ServiceUtil.returnSuccess();
   	
-  	result .put("msgUpper", msgUpper);
+  	result.put("msgUpper", msgUpper);
   	
   	return result ;
   	
+  }
+  
+  public static Map<String, Object> createRecipe (DispatchContext dctx, Map<String, Object> context)
+  {                                 
+    
+    // Use the following to access entities or other services of ofbiz
+    // Delegator delegator = dctx.getDelegator () ;
+    // LocalDispatcher dispatcher = dctx.getDispatcher () ;
+    
+    // Get the input parameters
+    String recipeName = (String)context.get("recipeName");
+    String notes = (String)context.get("notes");
+    String recipeTypeId = (String)context.get("recipeTypeId");
+    
+    String recipe = "r005;" + recipeName + ";" + notes + ";" + recipeTypeId + ";";
+            
+    Map<String, Object> result = ServiceUtil.returnSuccess();
+    result.put("recipe", recipe);
+    
+    return result;
+    
   }
   
 }
