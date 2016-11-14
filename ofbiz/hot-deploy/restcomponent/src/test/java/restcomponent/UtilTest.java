91cf49934f82a2481ff801c6df420cabe6173644
1010836 <1010836@isep.ipp.pt>
2016-11-14 23:06:57 +0000

    ODSOFT - Exercise 6 - ref #10
    Added the last point of the exercise: Publishing tests

---
+++ ofbiz/hot-deploy/restcomponent/src/test/java/restcomponent/UtilTest.java
@@ -0,0 +1,29 @@
+package restcomponent;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.ofbiz.entity.GenericValue;
+import org.junit.Test;
+
+public class UtilTest 
+{
+  
+  @Test
+  public void testConvertProdutSampleListGenericValueToJSON() 
+  {
+    
+    String expectedResult= "[{\"_ENTITY_NAME_\":\"EntityDummy\",\"_DELEGATOR_NAME_\":\"GenericDelegatorDummy\"}]";
+    
+    GenericValue aGenValue=new GenericValueDummy();
+    List<GenericValue> listIn=new ArrayList<GenericValue>();
+    listIn.add(aGenValue);
+    
+    String result=Util.convertListGenericValueToJSON(listIn);
+    
+    assertEquals("Teste: ", expectedResult, result);
+    
+  }
+
+}
